002b2fd6ebb25ebe6145fb64dc5911cf83aa6e4c
c.magyar <c.magyar.ec@gmail.com>
2018-04-21 23:49:03 -0500

    changed electroneum filenames and references

--- tests/libwallet_api_tests/scripts/open_wallet_miner.sh
+++ tests/libwallet_api_tests/scripts/open_wallet_miner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rlwrap electroneum-wallet-cli --wallet-file wallet_m --password "" --testnet --trusted-daemon --daemon-address 127.0.0.1:38081  --log-file wallet_m.log
+rlwrap stellite-wallet-cli --wallet-file wallet_m --password "" --testnet --trusted-daemon --daemon-address 127.0.0.1:38081  --log-file wallet_m.log
 
