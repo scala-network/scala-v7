a87a9800b27fd2b41496dc6678d910f59ec5be28
micmac <emichel31@gmail.com>
2018-05-12 08:31:02 +0200

    lower treshold for tendance activation

--- src/cryptonote_basic/difficulty.cpp
+++ src/cryptonote_basic/difficulty.cpp
@@ -346,12 +346,12 @@ difficulty_type next_difficulty_v4(std::vector<std::uint64_t> timestamps, std::v
 	difficulty_type median_last = epee::misc_utils::median(last_diffs);
 
 // CHECK TENDNACE
-	if(median_first > (median_mid*3/2) && median_mid > (median_last*3/2))
+	if(median_first > (median_mid*5/4) && median_mid > (median_last*4/3))
   {
 		timestamps.resize(30);
       		cumulative_difficulties.resize(30);
 	}
-	else if(median_mid > (median_first*3/2) && median_last > (median_mid*3/2)){
+	else if(median_mid > (median_first*5/4) && median_last > (median_mid*4/3)){
 		timestamps.resize(30);
   		cumulative_difficulties.resize(30);
 	}
