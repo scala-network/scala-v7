75513c60d9886d7ee75d2dcbbd57edfa00813965
hayzamjs <hayzam@gmail.com>
2018-10-16 21:28:32 +0000

    Add stable modules

---
+++ external/unbound/CMakeFiles/unbound.dir/DependInfo.cmake
@@ -0,0 +1,106 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/Stellite/external/unbound/compat/arc4_lock.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/compat/arc4_lock.c.o"
+  "/root/Stellite/external/unbound/compat/arc4random.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/compat/arc4random.c.o"
+  "/root/Stellite/external/unbound/compat/arc4random_uniform.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/compat/arc4random_uniform.c.o"
+  "/root/Stellite/external/unbound/compat/explicit_bzero.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/compat/explicit_bzero.c.o"
+  "/root/Stellite/external/unbound/compat/getentropy_linux.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/compat/getentropy_linux.c.o"
+  "/root/Stellite/external/unbound/compat/reallocarray.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/compat/reallocarray.c.o"
+  "/root/Stellite/external/unbound/compat/strlcat.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/compat/strlcat.c.o"
+  "/root/Stellite/external/unbound/compat/strlcpy.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/compat/strlcpy.c.o"
+  "/root/Stellite/external/unbound/dns64/dns64.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/dns64/dns64.c.o"
+  "/root/Stellite/external/unbound/iterator/iter_delegpt.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iter_delegpt.c.o"
+  "/root/Stellite/external/unbound/iterator/iter_donotq.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iter_donotq.c.o"
+  "/root/Stellite/external/unbound/iterator/iter_fwd.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iter_fwd.c.o"
+  "/root/Stellite/external/unbound/iterator/iter_hints.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iter_hints.c.o"
+  "/root/Stellite/external/unbound/iterator/iter_priv.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iter_priv.c.o"
+  "/root/Stellite/external/unbound/iterator/iter_resptype.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iter_resptype.c.o"
+  "/root/Stellite/external/unbound/iterator/iter_scrub.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iter_scrub.c.o"
+  "/root/Stellite/external/unbound/iterator/iter_utils.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iter_utils.c.o"
+  "/root/Stellite/external/unbound/iterator/iterator.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/iterator/iterator.c.o"
+  "/root/Stellite/external/unbound/libunbound/context.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/libunbound/context.c.o"
+  "/root/Stellite/external/unbound/libunbound/libunbound.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/libunbound/libunbound.c.o"
+  "/root/Stellite/external/unbound/libunbound/libworker.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/libunbound/libworker.c.o"
+  "/root/Stellite/external/unbound/respip/respip.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/respip/respip.c.o"
+  "/root/Stellite/external/unbound/services/authzone.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/authzone.c.o"
+  "/root/Stellite/external/unbound/services/cache/dns.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/cache/dns.c.o"
+  "/root/Stellite/external/unbound/services/cache/infra.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/cache/infra.c.o"
+  "/root/Stellite/external/unbound/services/cache/rrset.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/cache/rrset.c.o"
+  "/root/Stellite/external/unbound/services/listen_dnsport.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/listen_dnsport.c.o"
+  "/root/Stellite/external/unbound/services/localzone.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/localzone.c.o"
+  "/root/Stellite/external/unbound/services/mesh.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/mesh.c.o"
+  "/root/Stellite/external/unbound/services/modstack.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/modstack.c.o"
+  "/root/Stellite/external/unbound/services/outbound_list.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/outbound_list.c.o"
+  "/root/Stellite/external/unbound/services/outside_network.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/outside_network.c.o"
+  "/root/Stellite/external/unbound/services/view.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/services/view.c.o"
+  "/root/Stellite/external/unbound/sldns/keyraw.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/sldns/keyraw.c.o"
+  "/root/Stellite/external/unbound/sldns/parse.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/sldns/parse.c.o"
+  "/root/Stellite/external/unbound/sldns/parseutil.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/sldns/parseutil.c.o"
+  "/root/Stellite/external/unbound/sldns/rrdef.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/sldns/rrdef.c.o"
+  "/root/Stellite/external/unbound/sldns/sbuffer.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/sldns/sbuffer.c.o"
+  "/root/Stellite/external/unbound/sldns/str2wire.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/sldns/str2wire.c.o"
+  "/root/Stellite/external/unbound/sldns/wire2str.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/sldns/wire2str.c.o"
+  "/root/Stellite/external/unbound/testcode/checklocks.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/testcode/checklocks.c.o"
+  "/root/Stellite/external/unbound/util/alloc.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/alloc.c.o"
+  "/root/Stellite/external/unbound/util/as112.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/as112.c.o"
+  "/root/Stellite/external/unbound/util/config_file.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/config_file.c.o"
+  "/root/Stellite/external/unbound/util/configlexer.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/configlexer.c.o"
+  "/root/Stellite/external/unbound/util/configparser.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/configparser.c.o"
+  "/root/Stellite/external/unbound/util/data/dname.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/data/dname.c.o"
+  "/root/Stellite/external/unbound/util/data/msgencode.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/data/msgencode.c.o"
+  "/root/Stellite/external/unbound/util/data/msgparse.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/data/msgparse.c.o"
+  "/root/Stellite/external/unbound/util/data/msgreply.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/data/msgreply.c.o"
+  "/root/Stellite/external/unbound/util/data/packed_rrset.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/data/packed_rrset.c.o"
+  "/root/Stellite/external/unbound/util/fptr_wlist.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/fptr_wlist.c.o"
+  "/root/Stellite/external/unbound/util/locks.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/locks.c.o"
+  "/root/Stellite/external/unbound/util/log.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/log.c.o"
+  "/root/Stellite/external/unbound/util/mini_event.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/mini_event.c.o"
+  "/root/Stellite/external/unbound/util/module.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/module.c.o"
+  "/root/Stellite/external/unbound/util/net_help.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/net_help.c.o"
+  "/root/Stellite/external/unbound/util/netevent.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/netevent.c.o"
+  "/root/Stellite/external/unbound/util/random.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/random.c.o"
+  "/root/Stellite/external/unbound/util/rbtree.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/rbtree.c.o"
+  "/root/Stellite/external/unbound/util/regional.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/regional.c.o"
+  "/root/Stellite/external/unbound/util/rtt.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/rtt.c.o"
+  "/root/Stellite/external/unbound/util/storage/dnstree.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/storage/dnstree.c.o"
+  "/root/Stellite/external/unbound/util/storage/lookup3.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/storage/lookup3.c.o"
+  "/root/Stellite/external/unbound/util/storage/lruhash.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/storage/lruhash.c.o"
+  "/root/Stellite/external/unbound/util/storage/slabhash.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/storage/slabhash.c.o"
+  "/root/Stellite/external/unbound/util/timehist.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/timehist.c.o"
+  "/root/Stellite/external/unbound/util/tube.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/tube.c.o"
+  "/root/Stellite/external/unbound/util/ub_event.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/ub_event.c.o"
+  "/root/Stellite/external/unbound/util/winsock_event.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/util/winsock_event.c.o"
+  "/root/Stellite/external/unbound/validator/autotrust.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/autotrust.c.o"
+  "/root/Stellite/external/unbound/validator/val_anchor.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_anchor.c.o"
+  "/root/Stellite/external/unbound/validator/val_kcache.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_kcache.c.o"
+  "/root/Stellite/external/unbound/validator/val_kentry.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_kentry.c.o"
+  "/root/Stellite/external/unbound/validator/val_neg.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_neg.c.o"
+  "/root/Stellite/external/unbound/validator/val_nsec.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_nsec.c.o"
+  "/root/Stellite/external/unbound/validator/val_nsec3.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_nsec3.c.o"
+  "/root/Stellite/external/unbound/validator/val_secalgo.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_secalgo.c.o"
+  "/root/Stellite/external/unbound/validator/val_sigcrypt.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_sigcrypt.c.o"
+  "/root/Stellite/external/unbound/validator/val_utils.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/val_utils.c.o"
+  "/root/Stellite/external/unbound/validator/validator.c" "/root/Stellite/external/unbound/CMakeFiles/unbound.dir/validator/validator.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
